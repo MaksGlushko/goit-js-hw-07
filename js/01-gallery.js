01809b0463110a703a923acff8acac337261c412
MaksGlushko <maks2007glyshko2@gmail.com>
2023-09-28 16:43:46 +0200

    Homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,24 @@
-import { galleryItems } from './galleryitems.js';
-// // Change code below this line
-import * as basicLightbox from 'basiclightbox';
-
-
-const listEl = document.querySelector('.gallery')
-
-const typset = galleryItems.map(({ preview, original, description }) => {
-    return `<li  style="margin: 20px;" class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      loading="lazy"
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>` }).join('');
-
-listEl.insertAdjacentHTML("beforeend", typset)
-
 import { galleryItems } from './gallery-items.js';
 import * as basicLightbox from 'basiclightbox';
 
 
+const listEl = document.querySelector('.gallery');
+
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `<li style="margin: 20px;" class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                loading="lazy"
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+}).join('');
+
+listEl.insertAdjacentHTML("beforeend", typset);
 
 const modalOpen = (event) => {
     event.preventDefault();
@@ -31,37 +26,34 @@ const modalOpen = (event) => {
         console.log('NEpraça');
         return;
     }
-    console.log('praça');
+
     const clickedImageAlt = event.target.getAttribute('alt');
     const clickedImageSrc = event.target.dataset.source;
 
-
-
     const instance = basicLightbox.create(
         `<img src='${clickedImageSrc}' alt='${clickedImageAlt}'/>`, {
-        onShow: (instance) => {
-            document.addEventListener('keydown', modalClose)
-        },
-        onClose: (instance) => {
-            document.removeEventListener('keydown', modalClose);
-            instance = null;
-        }
-    });
-    const modalClose = (event) => {
-        if (event.key !== 'Escape') {
-            return
-        }
-        return instance.close();
-    }
-
+            onShow: (instance) => {
+                document.addEventListener('keydown', modalClose);
+            },
+            onClose: (instance) => {
+                document.removeEventListener('keydown', modalClose);
+                instance = null;
+            }
+        });
 
     instance.show();
-}
+};
 
+const modalClose = (event) => {
+    if (event.key !== 'Escape') {
+        return;
+    }
+    instance.close();
+};
 
-
+document.addEventListener('keydown', modalClose);
 
 listEl.addEventListener('click', (event) => {
-    modalOpen(event)
-})
+    modalOpen(event);
+});
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
 
-// // Change code below this line
-
-const listEl = document.querySelector('.gallery')
-
-const typset = galleryItems.map(({ preview, original, description }) => {
+const markup = galleryItems.map(({ preview, original, description }) => {
     return `
-<li style="margin: 20px;" class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>
-`}).join('');
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    </li>
+    `;
+}).join('');
 
-listEl.insertAdjacentHTML("beforeend", typset)
+listEl.insertAdjacentHTML("beforeend", markup);
 
 let gallery = new SimpleLightbox('.gallery a', {
     captions: true,
